426fcf6d3fd3c3902fa0b87b7b49e14f5eb1a7e2
anilankemstudy <68017059+anilankemstudy@users.noreply.github.com>
2026-03-06 22:33:01 +0530

    Update main.tf

--- terraform-dns/main.tf
+++ terraform-dns/main.tf
@@ -1,29 +1,28 @@
 resource "proxmox_lxc" "ubuntu_lxc" {
-  node_name = var.node_name         # Name of your Proxmox node
-  vm_id     = 8001               # Unique VM ID
+  node_name = var.node_name
+  vm_id     = 8001
   hostname  = "dns-server"
 
   ostemplate = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
 
   cores  = 2
-  memory = 2048                 # 2GB RAM
-  swap   = 512                  # Optional swap
+  memory = 2048
+  swap   = 512
 
   rootfs {
-    storage = "local"       # Your storage name
+    storage = "local"
     size    = "8G"
   }
 
- network {
-  name   = "eth0"
-  bridge = "vmbr0"
-  ip     = "192.168.1.40/24"  # Your static IP with subnet mask
-  gw     = "192.168.1.1"       # Optional: default gateway
-}
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "192.168.1.40/24"
+    gw     = "192.168.1.1"
+  }
 
-  # SSH key and root password
-  ssh_keys     = [var.ssh_public_key]
-  password     = var.root_password
-  lock         = false
-  start        = true           # Start container after creation
+  ssh_keys = [var.ssh_public_key]   # Inject SSH key
+  password = var.root_password      # Root password
+  lock     = false
+  start    = true
 }
