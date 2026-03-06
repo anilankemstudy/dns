18d9e70fb6a8885a4841010119c7ec9ac7b13949
anilankemstudy <68017059+anilankemstudy@users.noreply.github.com>
2026-03-06 22:30:37 +0530

    Update variables.tf

--- terraform-dns/variables.tf
+++ terraform-dns/variables.tf
@@ -2,16 +2,13 @@ variable "proxmox_api_url" {
   type = string
 }
 
+variable "proxmox_api_token_id" {
+  type = string
+}
+
 variable "proxmox_api_token" {
   type = string
-  sensitive = true
 }
-
-variable "node_name" {
-  type = string
-  default = "proxmox"
-}
-
 
 variable "ssh_public_key" {}
 variable "root_password" {}
