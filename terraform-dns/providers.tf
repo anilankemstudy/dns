a7fe49fec42adb4cdc44cb9a617ddbff042daefe
anilankemstudy <68017059+anilankemstudy@users.noreply.github.com>
2026-03-06 20:29:02 +0530

    Create providers.tf

---
+++ terraform-dns/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.96.0"
+
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint = var.proxmox_api_url
+  api_token = var.proxmox_token
+  insecure = true
+
+}
