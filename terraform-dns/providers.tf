8f3ecc66d463a9b1ab30e122259e6ffaef4436b7
anilankemstudy <anilankemstudy@gmail.com>
2026-03-07 00:05:28 +0530

    Remove Proxmox authentication method variable and refactor buildns.yml and providers.tf to exclusively use username/password for authentication

--- terraform-dns/providers.tf
+++ terraform-dns/providers.tf
@@ -7,18 +7,12 @@ terraform {
   }
 }
 
-locals {
-  use_token = var.proxmox_auth_method == "token"
-}
-
 provider "proxmox" {
   pm_api_url             = var.proxmox_api_url
 
-  # Auth (use either token OR user/password; leave the other as null)
-  pm_api_token_id        = local.use_token ? var.proxmox_api_token_id : null
-  pm_api_token_secret    = local.use_token ? var.proxmox_api_token_secret : null
-  pm_user                = local.use_token ? null : var.proxmox_user
-  pm_password            = local.use_token ? null : var.proxmox_password
+  # Auth via username/password (per provider docs)
+  pm_user                = var.proxmox_user
+  pm_password            = var.proxmox_password
 
   pm_tls_insecure        = true
 }
--- terraform-dns/variables.tf
+++ terraform-dns/variables.tf
@@ -2,28 +2,6 @@ variable "proxmox_api_url" {
   type = string
 }
 
-variable "proxmox_auth_method" {
-  type    = string
-  default = "password"
-
-  validation {
-    condition     = contains(["password", "token"], var.proxmox_auth_method)
-    error_message = "proxmox_auth_method must be \"password\" or \"token\"."
-  }
-}
-
-variable "proxmox_api_token_id" {
-  type      = string
-  default   = null
-  sensitive = true
-}
-
-variable "proxmox_api_token_secret" {
-  type      = string
-  default   = null
-  sensitive = true
-}
-
 variable "proxmox_user" {
   type    = string
   default = null
